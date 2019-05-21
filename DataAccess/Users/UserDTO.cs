eb00764369f2d8a84530ad37c6d5f9a66de121d6
linlijian <praw__Rul2__lvluk@Outlook.co.th>
2019-05-21 14:48:40 +0700

    NEW MAIN

---
+++ DataAccess/Users/UserDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UtilityLib;
+using DataAccess.SEC;
+
+namespace DataAccess.Users
+{
+    [Serializable]
+    public class UserDTO : BaseDTO
+    {
+        public UserDTO()
+        {
+            Model = new UserModel();
+            Notification = new NotificationModel();
+        }
+        public UserModel Model { get; set; }
+        public List<ModuleModel> ConfigGerarals { get; set; }
+        public List<AppModel> Apps { get; set; }
+        public List<NotificationModel> Notifications { get; set; }
+        public NotificationModel Notification { get; set; }
+    }
+
+    public class UserExecuteType : DTOExecuteType
+    {
+        public const string GetUser = "GetUser"; 
+        public const string GetConfigGeraral = "GetConfigGeraral";
+        public const string GetConfigSys = "GetConfigSys";
+        public const string GetApp = "GetApp";
+        public const string GetNotification = "GetNotification";
+        public const string GetNotificationCount = "GetNotificationCount";
+        public const string FatchNotification = "FatchNotification";
+        public const string UpdateFlag = "UpdateFlag";
+        
+    }
+
+    
+    //UserDA dal = new UserDA();
+    //UserDTO dto = new UserDTO();
+    //dto.Query.ExecuteType = UserExecuteType.GetByInRelateID;
+    //        dal.Select(dto);
+}
---
+++ DataAccess/Users/UserModel.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.SEC;
+using FluentValidation;
+using FluentValidation.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Users
+{
+    [Serializable]
+    public partial class UserModel : StandardModel
+    {
+        public string USER_ID { get; set; }
+        public string USER_FNAME_TH { get; set; }
+        public string USER_LNAME_TH { get; set; }
+        public string USER_FNAME_EN { get; set; }
+        public string USER_LNAME_EN { get; set; }
+        public string USER_NAME_EN { get; set; }
+        public string USER_NAME_TH { get; set; }
+        public Nullable<decimal> USG_ID { get; set; }
+        public string SYS_GROUP_NAME { get; set; }
+        public string COM_NAME_T { get; set; }
+        public string COM_NAME_E { get; set; }
+        public string USG_LEVEL { get; set; }
+    }
+
+    public class AppModel
+    {
+        public string COM_CODE { get; set; }
+        public string USER_ID { get; set; }
+        public int? USG_ID { get; set; }
+        public string COM_NAME { get; set; }
+        public string COM_NAME_E { get; set; }
+        public string COM_NAME_T { get; set; }
+    }
+
+    public class NotificationModel
+    {
+        public string COM_CODE { get; set; }
+        public string NO { get; set; }
+        public string NTF_KEY { get; set; }
+        public string USER_ID { get; set; }
+        public string DETAIL { get; set; }
+        public string FLAG { get; set; }
+        public string ACTIVE { get; set; }
+        public DateTime CRET_DATE { get; set; }
+        public DateTime MNT_DATE { get; set; }
+    }
+
+}
